e1f960324bbc387575438c8d4b907d7a601ac857
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-11-12 13:36:37 -0500

    TABSTOP DONE

--- MainWindow/resource.h
+++ MainWindow/resource.h
@@ -6,6 +6,9 @@
 #define IDI_ICON1                       1
 #define IDI_ICON2                       103
 
+#define IDC_STATIC						1000
+#define IDC_EDIT						1001
+#define IDC_BUTTON						1002
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
